5ac8a026eb513bde1787484e5d78945f054bf2a7
Andrea Funtò <d093154@xpia040.utenze.bankit.it>
2025-10-29 11:48:07 +0100

    test with archetype tool

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ func main() {
 
 	defer cleanup()
 
+	{{// this will be a comment indicating where to put subcommands }}
 	if len(os.Args) > 1 && os.Args[1] == "with" {
 		main_with_subcommands()
 	} else if len(os.Args) > 1 && os.Args[1] == "without" {
